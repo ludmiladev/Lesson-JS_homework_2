cfc95e8cf2971199fb7928ee0cb37b79c30fab94
ludmiladev <ljuda12@gmail.com>
2023-02-04 14:15:39 +0300

    дз перебрати масив вручну

--- main.js
+++ main.js
@@ -343,5 +343,47 @@
 // let x = 'sos' || 'default';
 // console.log(x);
 
-/*- з файлу arrays.js (лежати в папці 2023 plan ) взяти масив coursesAndDurationArray.
-За допомогою перевірте кожен його елемент на тривалість навчання. У разі якщо тривалість довша за 5 місяців виведіть у консоль "Супер".*/
+/*- - з файлу arrays.js (лежить в папці 2023 plan ) взяти масив coursesAndDurationArray.
+ За допомоги іф перевірити кожен його елемент на тривалість навчання.
+У випадку якщо тривалість довша за 5 місяців вивести в консоль "Супер".
+.*/
+
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+
+if (coursesAndDurationArray[0].monthDuration >= 5){
+    console.log(coursesAndDurationArray[0], 'Супер')
+}else {
+    console.log('Навчання менше 5 місяців')
+}
+if (coursesAndDurationArray[1].monthDuration >= 5){
+    console.log(coursesAndDurationArray[1], 'Супер')
+}else {
+    console.log('Навчання менше 5 місяців')
+}
+if (coursesAndDurationArray[2].monthDuration >= 5){
+    console.log(coursesAndDurationArray[2], 'Супер')
+}else {
+    console.log('Навчання менше 5 місяців')
+}
+if (coursesAndDurationArray[3].monthDuration >= 5){
+    console.log(coursesAndDurationArray[3], 'Супер')
+}else {
+    console.log('Навчання менше 5 місяців')
+}
+if (coursesAndDurationArray[4].monthDuration >= 5){
+    console.log(coursesAndDurationArray[4], 'Супер')
+}else {
+    console.log('Навчання менше 5 місяців')
+}
+if (coursesAndDurationArray[5].monthDuration >= 5){
+    console.log(coursesAndDurationArray[5], 'Супер')
+} else {
+    console.log('Навчання менше 5 місяців')
+}
