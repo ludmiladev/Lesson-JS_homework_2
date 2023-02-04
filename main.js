1b1407b4d8e920928c03390876f1982f9dd709fc
ludmiladev <ljuda12@gmail.com>
2023-02-04 14:23:43 +0300

    дз перебрати масив вручну

--- main.js
+++ main.js
@@ -348,42 +348,42 @@
 У випадку якщо тривалість довша за 5 місяців вивести в консоль "Супер".
 .*/
 
-let coursesAndDurationArray = [
-    {title: 'JavaScript Complex', monthDuration: 5},
-    {title: 'Java Complex', monthDuration: 6},
-    {title: 'Python Complex', monthDuration: 6},
-    {title: 'QA Complex', monthDuration: 4},
-    {title: 'FullStack', monthDuration: 7},
-    {title: 'Frontend', monthDuration: 4}
-];
-
-if (coursesAndDurationArray[0].monthDuration >= 5){
-    console.log(coursesAndDurationArray[0], 'Супер')
-}else {
-    console.log('Навчання менше 5 місяців')
-}
-if (coursesAndDurationArray[1].monthDuration >= 5){
-    console.log(coursesAndDurationArray[1], 'Супер')
-}else {
-    console.log('Навчання менше 5 місяців')
-}
-if (coursesAndDurationArray[2].monthDuration >= 5){
-    console.log(coursesAndDurationArray[2], 'Супер')
-}else {
-    console.log('Навчання менше 5 місяців')
-}
-if (coursesAndDurationArray[3].monthDuration >= 5){
-    console.log(coursesAndDurationArray[3], 'Супер')
-}else {
-    console.log('Навчання менше 5 місяців')
-}
-if (coursesAndDurationArray[4].monthDuration >= 5){
-    console.log(coursesAndDurationArray[4], 'Супер')
-}else {
-    console.log('Навчання менше 5 місяців')
-}
-if (coursesAndDurationArray[5].monthDuration >= 5){
-    console.log(coursesAndDurationArray[5], 'Супер')
-} else {
-    console.log('Навчання менше 5 місяців')
-}
+// let coursesAndDurationArray = [
+//     {title: 'JavaScript Complex', monthDuration: 5},
+//     {title: 'Java Complex', monthDuration: 6},
+//     {title: 'Python Complex', monthDuration: 6},
+//     {title: 'QA Complex', monthDuration: 4},
+//     {title: 'FullStack', monthDuration: 7},
+//     {title: 'Frontend', monthDuration: 4}
+// ];
+//
+// if (coursesAndDurationArray[0].monthDuration >= 5){
+//     console.log(coursesAndDurationArray[0], 'Супер')
+// }else {
+//     console.log('Навчання менше 5 місяців')
+// }
+// if (coursesAndDurationArray[1].monthDuration >= 5){
+//     console.log(coursesAndDurationArray[1], 'Супер')
+// }else {
+//     console.log('Навчання менше 5 місяців')
+// }
+// if (coursesAndDurationArray[2].monthDuration >= 5){
+//     console.log(coursesAndDurationArray[2], 'Супер')
+// }else {
+//     console.log('Навчання менше 5 місяців')
+// }
+// if (coursesAndDurationArray[3].monthDuration >= 5){
+//     console.log(coursesAndDurationArray[3], 'Супер')
+// }else {
+//     console.log('Навчання менше 5 місяців')
+// }
+// if (coursesAndDurationArray[4].monthDuration >= 5){
+//     console.log(coursesAndDurationArray[4], 'Супер')
+// }else {
+//     console.log('Навчання менше 5 місяців')
+// }
+// if (coursesAndDurationArray[5].monthDuration >= 5){
+//     console.log(coursesAndDurationArray[5], 'Супер')
+// } else {
+//     console.log('Навчання менше 5 місяців')
+// }
